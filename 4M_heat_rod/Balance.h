23663455d1e44e9ea058f8ee11143ef7b62d3b82
_xndr <Alexrom52alexrom52nn@mail.ru>
2017-12-02 13:42:34 +0300

    Added a pattern for form. Changed a struct of project. Reduced main

--- 4M_heat_rod/Balance.h
+++ 4M_heat_rod/Balance.h
@@ -1,93 +1,99 @@
 #include <cmath>
 #define M_PI 3.14159265358979323846
+using std::cout;
+using std::cin;
+using std::endl;
 
-using namespace std;
+namespace _4M_heat_rod{
 
+static double	ksi = 0.5;
+static int		dim = 10;
+static double	_step = 1. / dim;
 
-void set_a(double ksi, double step, long double* a) {
+void set_a(long double* a) {
 	a[0] = 0.;
 	double x = 0.;
 	int i(1);
 	while (x <= ksi)
 	{
-		*(a + i) = 1. / ((1. / step) * ((long double)exp(x + step - 0.5) - (long double)exp(x - 0.5)));
+		*(a + i) = 1. / ((1. / _step) * ((long double)exp(x + _step - 0.5) - (long double)exp(x - 0.5)));
 		i++;
-		x += step;
+		x += _step;
 	}
 	while (x <= 1)
 	{
 		a[i] = 1.;
 		i++;
-		x += step;
+		x += _step;
 	}
 }
 
-void set_d(double ksi, double step, unsigned int dim, long double* d) {
+void set_d(long double* d) {
 	d[0] = 0.;
-	double x = 0.5 * step;
+	double x = 0.5 * _step;
 	int i(1);
-	while (x + step < ksi)
+	while (x + _step < ksi)
 	{
 		d[i] = 2.;
 		i++;
-		x += step;
+		x += _step;
 	}
 	i++;
-	d[i] = (1. / step) * (1. + (-(long double)cos(M_PI * (x + step) + (long double)cos(M_PI * (x + 0.5 * step)))));
-	x += step;
+	d[i] = (1. / _step) * (1. + (-(long double)cos(M_PI * (x + _step) + (long double)cos(M_PI * (x + 0.5 * _step)))));
+	x += _step;
 
-	while (x <= 1 - 0.5 * step)
+	while (x <= 1 - 0.5 * _step)
 	{
-		d[i] = (1. / step) * (-(long double)cos(M_PI * (x + step) + (long double)cos(M_PI * x)));
+		d[i] = (1. / _step) * (-(long double)cos(M_PI * (x + _step) + (long double)cos(M_PI * x)));
 		i++;
-		x += step;
+		x += _step;
 	}
 	d[dim] = 0.;
 }
 
-void set_Phi(double ksi, double step, unsigned int dim, long double* Phi) {
+void set_Phi(long double* Phi) {
 	Phi[0] = 1;
-	double x = 0.5 * step;
+	double x = 0.5 * _step;
 	int i(1);
-	while (x + step < ksi)
+	while (x + _step < ksi)
 	{
-		Phi[i] = -(1. / step) * ((long double)sin(M_PI * (x + step) - (long double)sin(M_PI * x)));
+		Phi[i] = -(1. / _step) * ((long double)sin(M_PI * (x + _step) - (long double)sin(M_PI * x)));
 		i++;
-		x += step;
+		x += _step;
 	}
 	i++;
-	Phi[i] = -(1. / step) * ((long double)sin(M_PI * (x + 0.5 * step) - (long double)sin(M_PI * x) +
-		((long double)exp(x + step - 0.5) - (long double)exp(x + 0.5 * step - 0.5))));
-	x += step;
+	Phi[i] = -(1. / _step) * ((long double)sin(M_PI * (x + 0.5 * _step) - (long double)sin(M_PI * x) +
+		((long double)exp(x + _step - 0.5) - (long double)exp(x + 0.5 * _step - 0.5))));
+	x += _step;
 
-	while (x <= 1 - 0.5 * step)
+	while (x <= 1 - 0.5 * _step)
 	{
-		Phi[i] = -(1. / step) * ((long double)exp(x + step - 0.5) - (long double)exp(x - 0.5));
+		Phi[i] = -(1. / _step) * ((long double)exp(x + _step - 0.5) - (long double)exp(x - 0.5));
 		i++;
-		x += step;
+		x += _step;
 	}
 	Phi[dim] = 1;
 }
 
-void set_A(double step, unsigned int dim, long double* a, long double* A) {
+void set_A(long double* a, long double* A) {
 	for (int i(0); i < dim; ++i) {
-		A[i] = a[i] / (step * step);
+		A[i] = a[i] / (_step * _step);
 	}
 	A[dim] = -1;
 }
 
-void set_B(double step, unsigned int dim, long double* a, long double* B) {
+void set_B(long double* a, long double* B) {
 	B[0] = -1.;
 	for (int i(1); i < dim; ++i) {
-		B[i] = a[i + 1] / (step * step);
+		B[i] = a[i + 1] / (_step * _step);
 	}
 }
 
-void set_C(double step, unsigned int dim, long double* a, long double* d, long double* C) {
+void set_C(long double* a, long double* d, long double* C) {
 	C[0] = 1;
 	C[dim] = 1;
 	for (int i(1); i < dim; ++i) {
-		C[i] = -(a[i] / (step * step) + a[i + 1] / (step * step) + d[i]);
+		C[i] = -(a[i] / (_step * _step) + a[i + 1] / (_step * _step) + d[i]);
 	}
 }
 
@@ -157,4 +163,5 @@ void rush(int n, double *_A, double *_C, double *_B, double *_Phi, double *_V)
 	for (int i = n - 2; i >= 0; i--)
 		_V[i] = (_Phi[i] - _B[i] * _V[i + 1]) / _C[i];
 
+}
 }
---
+++ 4M_heat_rod/__main.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//#include "Balance.h"
+//
+//using namespace _4M_heat_rod;
+//
+//static long double* a = new long double[dim + 1];
+//static long double* Phi = new long double[dim + 1];
+//static long double* d = new long double[dim + 1];
+//
+//long double* A = new long double[dim + 1];
+//long double* B = new long double[dim + 1];
+//long double* C = new long double[dim + 1];
+//
+//
+//
+//int main(int argc, char* argv[]) {
+//	set_a(a);
+//
+//	for (int i(0); i < dim + 1; ++i) {
+//		cout << a[i] << endl;
+//
+//	}
+//	cout << endl;
+//
+//	set_d(d);
+//
+//	for (int i(0); i < dim + 1; ++i) {
+//		cout << d[i] << endl;
+//
+//	}
+//
+//	cout << endl;
+//
+//	set_Phi(Phi);
+//
+//	for (int i(0); i < dim + 1; ++i) {
+//		cout << Phi[i] << endl;
+//
+//	}
+//
+//	
+//	system("pause");
+//	return 0;
+//}
